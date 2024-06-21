a26d50a0b6d13dc8bd8aa2492c40df20f4c07c57
Hanna Moazam <hannamoazam@gmail.com>
2024-06-21 23:22:01 +0100

    Testing script for download

--- .github/workflow_scripts/install_testpypi_pkg.sh
+++ .github/workflow_scripts/install_testpypi_pkg.sh
@@ -1,8 +1,10 @@
 #!/bin/bash  
-  
+
 # The $1 argument is the version number passed from the workflow  
-VERSION=$1  
-  
+VERSION=$1
+
+echo "version: $VERSION"
+
 for i in {1..5}; do  
   if python3 -m pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple dspy-ai-test=="$VERSION"; then  
     break  
