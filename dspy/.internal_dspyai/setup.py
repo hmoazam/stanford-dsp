3ea43697b2f39474393ef8a0fdee1530d1f8dbb3
hmoazam <hannamoazam@gmail.com>
2024-10-04 20:06:35 +0300

    testing

--- dspy/.internal_dspyai/setup.py
+++ dspy/.internal_dspyai/setup.py
@@ -4,10 +4,6 @@ from setuptools import find_packages, setup
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
-# Read the content of the requirements.txt file
-with open("requirements.txt", encoding="utf-8") as f:
-    requirements = f.read().splitlines()
-
 setup(	
     name="dspy-ai",
     #replace_package_version_marker
@@ -21,7 +17,7 @@ setup(
     license="MIT License",	
     packages=find_packages(include=["dsp.*", "dspy.*", "dsp", "dspy"]),	
     python_requires=">=3.9",
-    # replace_dspy_version_marker
+    #replace_dspy_version_marker
     install_requires=[  
         "dspy == 2.5.3",   
     ]
